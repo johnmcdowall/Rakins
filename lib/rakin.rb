651a298e74d53449c5e2d3b2a55745451d24bb92
Aanand Prasad <aanand.prasad@gmail.com>
2011-09-22 18:06:36 +0100

    Use 'system' instead of backticks
    This ensures that:
    - coloured output is preserved
    - the stdout of the exec'ed command is not buffered, but displayed
      immediately
    - arguments are automatically escaped

--- lib/rakin.rb
+++ lib/rakin.rb
@@ -28,14 +28,14 @@ class Rakin
 
     if !first_arg.nil? && first_arg=='install'
       puts Rakin.speaks "INSTALLIN' #{second_arg}, YO."
-      puts `gem install #{second_arg}`
+      system "gem", "install", second_arg
     elsif !first_arg.nil? && first_arg=='help'
       puts "rakins install <gem> will install a gem, rakins <command> will bundle exec the command."
     elsif !first_arg.nil? && first_arg=='origin'
-      `open http://www.youtube.com/watch?v=QHQ7bt5LBj8`
+      system "open", "http://www.youtube.com/watch?v=QHQ7bt5LBj8"
     elsif !first_arg.nil? && !second_arg.nil?
       puts Rakin.speaks "EXECIN' #{first_arg}, YO."
-      puts `bundle exec #{first_arg} #{second_arg}`
+      system "bundle", "exec", first_arg, second_arg
     elsif first_arg.nil?
       puts Rakin.talks_back
     end
