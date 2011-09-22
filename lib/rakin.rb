f3fd2f67171297c6ccfa4548511d8a5350ba91f9
Aanand Prasad <aanand.prasad@gmail.com>
2011-09-22 18:15:26 +0100

    Clean up command parser

--- lib/rakin.rb
+++ lib/rakin.rb
@@ -26,14 +26,15 @@ class Rakin
     first_arg  = ARGV[0]
     second_arg = ARGV[1]
 
-    if first_arg.nil?
+    case first_arg
+    when nil
       puts Rakin.talks_back
-    elsif !first_arg.nil? && first_arg=='install'
+    when 'install'
       puts Rakin.speaks "INSTALLIN' #{second_arg}, YO."
       system "gem", "install", second_arg
-    elsif !first_arg.nil? && first_arg=='help'
+    when 'help'
       puts "rakins install <gem> will install a gem, rakins <command> will bundle exec the command."
-    elsif !first_arg.nil? && first_arg=='origin'
+    when 'origin'
       system "open", "http://www.youtube.com/watch?v=QHQ7bt5LBj8"
     else
       puts Rakin.speaks "EXECIN' #{first_arg}, YO."
